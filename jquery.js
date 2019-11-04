449c0976deadfc1903d2028e3d3525203f641cab
B-Maher <bouzid.meher@gmail.com>
2019-11-04 14:51:10 +0100

    M 01 Jquery

---
+++ jquery.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+	var count = 0;
+	$('button').on('click', function(){
+	var id = $(':checked')[0].id
+	console.log(document.getElementsByClassName(id)[0].innerText)
+	/*if(document.getElementsByClassName(id)[0].innerText === answer){
+		count++
+	}*/
+})	
+})
