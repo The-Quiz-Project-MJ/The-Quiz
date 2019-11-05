1535604b35528e49d655274f4b8c05a5749959fe
jihedabdelly <jihednew7@gmail.com>
2019-11-05 15:38:48 +0100

    J 04

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var arrayOfCountries = ["Argentina", "Qatar", "Lebanon", "Austria","Malaysia", "Egypt", "Angola", "Jordan", "Syria", "Brazil", "Saudi-Arabia", "Malaysia", "Denmark", "Vietnam", "Iran", "Cambodia", "Kazakhstan", "India", "China", "Bahrain", "Palestine", "Bhutan", "Afghanistan", "Poland", "Belgium", "Canada", "Mexico", "Madagascar", "Niger", "Australia", "Tunisia", "Spain", "Bangladesh", "Oman", "Nepal", "Libya", "Jamaica", "Ghana", "Cuba", "Peru", "Croatia", "Korea-North", "Estonia"]
+var arrayOfCountries = ["Argentina", "Qatar", "Lebanon", "Austria","Malaysia", "Egypt", "Angola", "Jordan", "Syria", "Brazil", "Saudi-Arabia", "Malaysia", "Denmark", "Iran", "Cambodia", "Kazakhstan", "India", "China", "Bahrain", "Palestine", "Bhutan", "Afghanistan", "Poland", "Belgium", "Canada", "Mexico", "Madagascar", "Niger", "Australia", "Tunisia", "Spain", "Bangladesh", "Oman", "Libya", "Jamaica", "Ghana", "Cuba", "Peru", "Croatia", "Korea-North", "Estonia"]
 
 function objCreator(array) {
 	var arrayOfObjects = [];
@@ -14,10 +14,10 @@ function objCreator(array) {
 }
 
 var questions = objCreator(arrayOfCountries);
+
 function randomNumberGenerator(array){
 	return Math.floor(Math.random() * array.length)	
 }
-
 
 function nextQuestion(questionNumber){
 	questions.splice(questionNumber, 1)
@@ -40,6 +40,33 @@ function generateAnswers(array,n){
 	}
 	return newArray.sort();
 }
+
+function hide() {
+	$('.question').hide();
+	$('#answers').hide();
+	//$('img').hide();
+}
+
+function outputMesaage(result) {
+	var percentage = result*5;
+	var message = "";
+	if(percentage < 20) {
+		return "You Suck You only made "+ percentage + " %"
+	} else if(percentage < 20) {
+		return "You Suck You only made "+ percentage + " %"
+	} else if(percentage < 40) {
+		return "You Suck You only made "+ percentage + " %"
+	} else if(percentage < 60) {
+		return "You Suck You only made "+ percentage + " %"
+	} else if(percentage < 20) {
+		return "You Suck You only made "+ percentage + " %"
+	} else if(percentage < 20) {
+		return "You Suck You only made "+ percentage + " %"
+	} else {
+
+	}
+}
+
 function displayAnswers(array){
 	var i = 0;
 	while(i < array.length){
@@ -51,36 +78,46 @@ function displayAnswers(array){
 var questionNumber = randomNumberGenerator(questions)
 var a = 0;
 var result = 0;
-var id = 1
-	$('#start').on('click', function(){
-		$('.question').show()
-		$('#answers').show()
-		$('#start').hide()
-		questionNumber = randomNumberGenerator(questions)
-		src = questions[questionNumber].url() 
-		$('#question img').attr('src',src)
-		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
-	})
-	$('#next').on('click', function(){
-		id = $(':checked')[0].id
-		if(document.getElementsByClassName(id)[0].innerText === questions[questionNumber].name){
-			++result
-		}
-		if(a === 19){
-			console.log('aaaaaaaaaaaaaa')
-			alert('Your final score is : ' + result)
-		}
-		++a
-		console.log(a, result)
-		$(':checked')[0].checked = false;
-		nextQuestion(questionNumber)
-		questionNumber = randomNumberGenerator(questions)
-		src = questions[questionNumber].url() 
-		$('#question img').attr('src',src)
-		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
-	});
+var id = 1;
+var src = "";
+var percentage = "";
+ 	
+$('#start').on('click', function(){
+	$('.question').show()
+	$('#answers').show()
+	//$('img').show();
+	$('#start').hide()
+	questionNumber = randomNumberGenerator(questions)
+	src = questions[questionNumber].url() 
+	$('#question img').attr('src',src)
+	displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
+})
 
-$(document).ready(function(){
-	$('.question').hide()
-	$('#answers').hide()
-})	
+$('#next').on('click', function(){
+	id = $(':checked')[0].id
+	if(document.getElementsByClassName(id)[0].innerText === questions[questionNumber].name){
+		++result
+	}
+	if(a === 5){
+		//console.log('aaaaaaaaaaaaaa')
+		//alert('Your final score is : ' + result)
+		percentage = result*5  
+		
+		var newStr = 'Your result is : ' + percentage + " %"
+		//alert('Your score ' + newStr)
+		//$('section #question').hide()
+		$('section #question').text(newStr)
+	     
+	}
+	++a
+	console.log(a, result)
+	$(':checked')[0].checked = false;
+	nextQuestion(questionNumber)
+	questionNumber = randomNumberGenerator(questions)
+	src = questions[questionNumber].url() 
+	$('#question img').attr('src',src)
+	displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
+});
+
+$(document).ready(hide())	
+
