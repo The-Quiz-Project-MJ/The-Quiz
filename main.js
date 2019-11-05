1f178914126559816fb8d06ae27f8af76a24c7c4
B.Maher <bouzid.meher@gmail.com>
2019-11-05 11:30:53 +0100

    M 05 js almost done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var arrayOfCountries = ["Argentina","Angola","Brazil","Belgium","Canada","Mexico","Madagascar","Niger","Australia", "Tunisia","Spain","Oman","Libya","Jamaica","Ghana" ,"Cuba","Peru","Croatia","Korea-North","Estonia"]
+var arrayOfCountries = ["Argentina", "Qatar", "Lebanon", "Austria","Malaysia", "Egypt", "Angola", "Jordan", "Syria", "Brazil", "Saudi-Arabia", "Malaysia", "Denmark", "Vietnam", "Iran", "Cambodia", "Kazakhstan", "India", "China", "Bahrain", "Palestine", "Bhutan", "Afghanistan", "Poland", "Belgium", "Canada", "Mexico", "Madagascar", "Niger", "Australia", "Tunisia", "Spain", "Bangladesh", "Oman", "Nepal", "Libya", "Jamaica", "Ghana", "Cuba", "Peru", "Croatia", "Korea-North", "Estonia"]
 
 function objCreator(array) {
 	var arrayOfObjects = [];
@@ -14,68 +14,73 @@ function objCreator(array) {
 }
 
 var questions = objCreator(arrayOfCountries);
-function questionGenerator(){
-	return Math.floor(Math.random() * questions.length)	
+function randomNumberGenerator(array){
+	return Math.floor(Math.random() * array.length)	
 }
 
 
 function nextQuestion(questionNumber){
 	questions.splice(questionNumber, 1)
 }
+
 function generateAnswers(array,n){
-	var copyArray = array;
+	var copyArray = array.map(function(elm){
+		return elm
+	});
+	var num = randomNumberGenerator(copyArray)
 	var newArray = [array[n]];
-	var num = questionGenerator();
-	function innerFunction(array, i){
-		i = i || 0;
-		console.log(i, array[num], num, n)
-		if(i === 5){
-			return newArray;
-		}
-		if (num ==! n){
-			newArray.push(array[num])
-			array.splice(num, 1)
-			return innerFunction(array, i + 1)
-		}
-		num = questionGenerator();
-		return innerFunction(array, i)
-	}
-	return innerFunction(copyArray).sort()
-/*	for(var i= 0; i < 6; i++){
-		var num = questionGenerator()
-		if (num ==! n){
-			newArray.push(array[i])
+	for(var i= 0; i < 5; i++){
+		num = randomNumberGenerator(copyArray)
+		if (copyArray[num] !== array[n]){
+			newArray.push(copyArray[num])
+			copyArray.splice(num, 1)
 		}else {
 			i--
 		}
 	}
-	return newArray.sort();*/
+	return newArray.sort();
 }
 function displayAnswers(array){
 	var i = 0;
 	while(i < array.length){
-		console.log(array)
 		document.getElementsByClassName(i + 1)[0].innerHTML = array[i];
 		i++
 	}
 }
 
-$(document).ready(function(){
-	var questionNumber = questionGenerator()
-	var src = questions[questionNumber].url() 
-	$('#question img').attr('src',src)
-	displayAnswers(generateAnswers(arrayOfCountries,questionNumber))
-	var count = 0;
-	$('button').on('click', function(){
-		nextQuestion(questionNumber)
-		questionNumber = questionGenerator()
+var questionNumber = randomNumberGenerator(questions)
+var a = 0;
+var result = 0;
+var id = 1
+	$('#start').on('click', function(){
+		$('.question').show()
+		$('#answers').show()
+		$('#start').hide()
+		questionNumber = randomNumberGenerator(questions)
 		src = questions[questionNumber].url() 
 		$('#question img').attr('src',src)
-		displayAnswers(generateAnswers(arrayOfCountries,questionNumber))
-		var id = $(':checked')[0].id
-	console.log(document.getElementsByClassName(id)[0].innerText)
-	/*if(document.getElementsByClassName(id)[0].innerText === answer){
-		count++
-	}*/
+		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
+	})
+	$('#next').on('click', function(){
+		id = $(':checked')[0].id
+		if(document.getElementsByClassName(id)[0].innerText === questions[questionNumber].name){
+			++result
+		}
+		if(a === 19){
+			console.log('aaaaaaaaaaaaaa')
+			alert('Your final score is : ' + result)
+		}
+		++a
+		console.log(a, result)
+		$(':checked')[0].checked = false;
+		nextQuestion(questionNumber)
+		questionNumber = randomNumberGenerator(questions)
+		src = questions[questionNumber].url() 
+		$('#question img').attr('src',src)
+		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
+	});
+
+$(document).ready(function(){
+	$('.question').hide()
+	$('#answers').hide()
 })	
-})
