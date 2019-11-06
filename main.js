563bfe5fd1581d145eabe608d0fd19c1d16b1ce8
B.Maher <bouzid.meher@gmail.com>
2019-11-06 10:49:15 +0100

    Final push for now

--- main.js
+++ main.js
@@ -16,14 +16,17 @@ function theQuiz(){
 		return arrayOfObjects;
 	}
 
+	//function that generate a random numbr between 0 and the length of the initial array;
 	function randomNumberGenerator(array){
 		return Math.floor(Math.random() * array.length)	
 	}
 
+	//function that remove a question from the question list
 	function nextQuestion(questionNumber){
 		questions.splice(questionNumber, 1)
 	}
 
+	//function that generate 5 random answers add the right answer to them and sort them alphabeticlly
 	function generateAnswers(array,n){
 		var copyArray = array.map(function(elm){
 			return elm
@@ -42,6 +45,7 @@ function theQuiz(){
 		return newArray.sort();
 	}
 	
+	//function that display the answers to the user
 	function displayAnswers(array){
 		var i = 0;
 		while(i < array.length){
@@ -50,27 +54,28 @@ function theQuiz(){
 		}
 	}
 
+	//function that will generate the final msg to the user
 	function outputMsg(res){
 		var percentage = Math.round((result * 100)/maxQuestion);
 		var msg = ""
 		if(percentage < 10){
-			return "You're a flag 'Ignorant' " + userName + ", you only made " + percentage + "%"
+			return "You're a flag 'Ignorant' " + userName + ", you only got " + percentage + "%"
 		}else if(percentage < 30){
-			return "You really don't know your flags "  + userName + ", you only made " + percentage + "%"
+			return "You really don't know your flags "  + userName + ", you only got " + percentage + "%"
 		}else if(percentage < 50){
-			return "Meh!!, " + userName + " you only made " + percentage + "%"
+			return "Meh!!, " + userName + " you only got " + percentage + "%"
 		}else if(percentage < 60){
-			return "You're OK " + userName + ", you made " + percentage + "%"
+			return "You're OK " + userName + ", you got " + percentage + "%"
 		}else if(percentage < 80){
-			return "You're Fine " + userName + ", you made " + percentage + "%"
+			return "You're Fine " + userName + ", you got " + percentage + "%"
 		}else if(percentage < 100){
-			return "You're good " + userName + ", you made " + percentage + "%"
+			return "You're good " + userName + ", you got " + percentage + "%"
 		}
-		return "You're a genius " + userName + ", you made " + percentage + "%"
+		return "You're a genius " + userName + ", you got " + percentage + "%"
 	}
 
-	var questions = objCreator(arrayOfCountries);
-	var questionNumber = randomNumberGenerator(questions);
+	var questions = objCreator(arrayOfCountries);//list of the questions
+	var questionNumber = randomNumberGenerator(questions);//the index of the question
 	var limit = 0;//number of the current question
 	var result = 0;
 	var id = 1;
@@ -78,12 +83,13 @@ function theQuiz(){
 	var src = "";
 	var maxQuestion = "";
 
-
+	//this will select the answer that the user click on
 	$('.clicked').on('click', function(){
 		var id = '#' + this.getAttribute("id")[3]
 		$(id)[0].checked = true
 	})
 
+	//this function will start the game by displaying the question and answer and hiding the introduction questions
 	$('#start').on('click', function(){
 		userName = $('#userName').val();
 		maxQuestion = $('#dropdown').val();
@@ -94,23 +100,28 @@ function theQuiz(){
 		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
 	})
 
+	//this function will display the next question and answers to the user
 	$('#next').on('click', function(){
 		if($(':checked')[1] === undefined){
 			alert('You need to choose an answer')
 		}
+
 		id = $(':checked')[1].id
 		if(document.getElementsByClassName(id)[0].innerText === questions[questionNumber].name){
 			++result
 		}
+
 		if(limit === maxQuestion - 2){
 		$('#next').text('Show Result')
 		}
+
 		if(limit === maxQuestion - 1){
 			finalScore();
 			return 1;
 		}
+
 		++limit
-		console.log(limit, result)
+		
 		$(':checked')[1].checked = false;
 		nextQuestion(questionNumber)
 		questionNumber = randomNumberGenerator(questions)
@@ -120,10 +131,12 @@ function theQuiz(){
 		displayAnswers(generateAnswers(arrayOfCountries,arrayOfCountries.indexOf(questions[questionNumber].name)))
 	});
 
+	//this function will play the game again
 	$('#playAgain').on('click',function(){
 		playAgain()
 	})
 
+	//this will display the final score and msg
 	function finalScore(){
 		hide();
 		$('#question').append('<p id="result">' + outputMsg(result) + '</p>')
